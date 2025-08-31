1ce81ae81c3f866486f16750fe5d59b05fb670b6
Oscar Rodriguez <118637313+cetusmx@users.noreply.github.com>
2025-08-30 23:03:37 -0600

    1.02

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3004;
+
+// Configure CORS to allow all origins. Quitar para produccion
+app.use(cors()); 
 
 app.use(express.json());
 
