4489b08dd8905632d02037db600a1d5147f6b60a
Oscar Rodriguez <118637313+cetusmx@users.noreply.github.com>
2025-10-26 01:42:15 -0600

    1.12

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "mysql",
-    "logging": false
+    "logging": true
   },
   "test": {
     "username": process.env.DB_USER,
@@ -23,6 +23,6 @@ module.exports = {
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "mysql",
-    "logging": false
+    "logging": true
   }
 };
